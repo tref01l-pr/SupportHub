03b5eb9568d273072dd0de950cbfda2b5a1628f9
Roman Protsai <protsayroman228@gmail.com>
2024-12-30 10:37:57 +0100

    Update

---
+++ SupportHubBackEnd/SupportHub.API/Contracts/Requests/User/UserRegistrationWithCompanyRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SupportHub.Domain.Models;
+
+namespace SupportHub.API.Contracts;
+
+public class UserRegistrationWithCompanyRequest : UserRegistrationRequest
+{
+    [Required]
+    [MaxLength(Company.MaxNameLength)]
+    public string CompanyName { get; set; }
+}
